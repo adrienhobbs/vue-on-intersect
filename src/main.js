629f666ef6dcbc7c4e19faf663e9a91063bc83c0
Adrien Hobbs <adrien.hobbs@gmail.com>
2018-10-22 14:23:22 -0400

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueOnIntersect from './lib/index'
+import Vuex from 'vuex'
 
+Vue.use(Vuex)
 Vue.use(VueOnIntersect)
 
 Vue.config.productionTip = false
