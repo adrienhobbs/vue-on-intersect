7715fc8f135f29d7e97bcc0e81ce35048ed0c19d
Adrien Hobbs <adrien.hobbs@gmail.com>
2018-10-25 12:22:36 -0400

    update imports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,6 @@
 import './polyfills'
 export { default as Observe } from './Observe'
-export { default } from './plugin'
 export { default as VueIntersectMixin } from './Mixin'
+
+import Plugin from './plugin'
+export default Plugin
